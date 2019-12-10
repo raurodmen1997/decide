51d41b5cc0841253c1eed333314b25a2a9e370e2
gonferjim <gonferjim@alum.us.es>
2019-12-10 12:28:27 +0100

    metodo leyParidad

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -4,6 +4,16 @@ from rest_framework.response import Response
 
 class PostProcView(APIView):
 
+    #atributo de candidato GENERO puede ser "F"=female o "M"=male
+    def seCumpleLeyParidad(lista):
+	    sumFemales = 0
+	    for x in lista:
+		    for y in x.values():
+			    if y == "F":
+				    sumFemales += 1
+	    return 0.4*len(lista) <= sumFemales <= 0.6*len(lista)
+
+
     def identity(self, options):
         out = []
 
