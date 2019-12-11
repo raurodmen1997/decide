20ef20619ed0856aea3164402b1b37a22d5f44bd
FraPazGal <frandepaz@yahoo.es>
2019-12-11 01:20:01 +0100

    [FEAT] COCIENTE HARE METODOS CORE
    Primera implementacion. Faltan métodos de formateo e integración.
    Utilizado un dict de ejemplo para el testeo.

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -1,19 +1,85 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import math
+import copy
 
 class PostProcView(APIView):
 
     def identity(self, options):
         out = []
+        numEscaños = 21
+
+        lista = {"A":391000, "B":312000, "C":245034, "D":200001, "E":98562, "F":20145}
+
+        def hareQuotient(votes):
+            q = math.floor(votes/numEscaños)
+            return q
+
+        def votesSum(lista):
+            sum = 0
+            for x in lista.values():
+                sum += x
+            return sum
+
+        def seatsAndResidues(lista):
+            res = {}
+            quotient = hareQuotient(votesSum(lista))
+            for x in lista.values():
+                a = []
+                seats = math.floor(x/quotient)
+                residue = x - quotient*seats
+
+                a.append(seats)
+                a.append(residue)
+
+                key_list = list(lista.keys()) 
+                val_list = list(lista.values()) 
+                n = key_list[val_list.index(x)]
+
+                res[n] = a
+
+            return res
+
+        def residueDistribution(initalDist):
+            distSeats = 0
+            n = 0
+            residues = []
+            finalDist = copy.deepcopy(initalDist)
+            values = finalDist.values()
+
+            for x in values:
+                distSeats += x[0]
+                residues.append(x[1])
+
+            notDistributed = numEscaños - distSeats
+            sortedResidues = residues.copy()
+            sortedResidues.sort(reverse = True)
+
+            while notDistributed > 0:
+                selected = sortedResidues[n]
+                pos = residues.index(selected)
+            
+                residues.pop(pos)
+                notDistributed -= 1
+
+                list(values)[pos][0] += 1
+
+                n += 1
+
+            return finalDist
+
+
+        results = residueDistribution(seatsAndResidues(lista))
 
         for opt in options:
             out.append({
                 **opt,
                 'postproc': opt['votes'],
-            });
+            })
 
         out.sort(key=lambda x: -x['postproc'])
+
         return Response(out)
 
     def post(self, request):
@@ -36,3 +102,5 @@ class PostProcView(APIView):
             return self.identity(opts)
 
         return Response({})
+
+    
