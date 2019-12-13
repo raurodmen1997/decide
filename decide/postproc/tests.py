6b043d2ad81567e2e4cd25ef2aefcdd16833e94d
Cristian <cristian_arahal11@hotmail.com>
2019-12-13 19:17:52 +0100

    [TEST] - [#10] - TEST COCIENTE IMPERIALI Mediante los test comprobaremos el comportamiento del método en diferentes casos de prueba.

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -42,3 +42,220 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+    def test_imperiali_1(self):
+        data = {
+            'type': 'COCIENTE_IMPERIALI1',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 391000 },
+                { 'option': 'B', 'number': 2, 'votes': 311000 },
+                { 'option': 'C', 'number': 3, 'votes': 184000 },
+                { 'option': 'D', 'number': 4, 'votes': 73000 },
+                { 'option': 'E', 'number': 5, 'votes': 27000 },
+                { 'option': 'F', 'number': 6, 'votes': 12000 },
+                { 'option': 'G', 'number': 7, 'votes': 2000 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'A', 'number': 1, 'votes': 391000, 'escanyos': 9},
+            { 'option': 'B', 'number': 2, 'votes': 311000, 'escanyos': 7},
+            { 'option': 'C', 'number': 3, 'votes': 184000, 'escanyos': 4},
+            { 'option': 'D', 'number': 4, 'votes': 73000, 'escanyos': 1},
+            { 'option': 'E', 'number': 5, 'votes': 27000, 'escanyos': 0},
+            { 'option': 'F', 'number': 6, 'votes': 12000, 'escanyos': 0},
+            { 'option': 'G', 'number': 7, 'votes': 2000, 'escanyos': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_imperiali_2(self):
+        #Test 2: Más partidos que escaños a repartir
+        data = {
+            'type': 'COCIENTE_IMPERIALI2',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 391000 },
+                { 'option': 'B', 'number': 2, 'votes': 311000 },
+                { 'option': 'C', 'number': 3, 'votes': 184000 },
+                { 'option': 'D', 'number': 4, 'votes': 73000 },
+                { 'option': 'E', 'number': 5, 'votes': 27000 },
+                { 'option': 'F', 'number': 6, 'votes': 12000 },
+                { 'option': 'G', 'number': 7, 'votes': 2000 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'A', 'number': 1, 'votes': 391000, 'escanyos': 3},
+            { 'option': 'B', 'number': 2, 'votes': 311000, 'escanyos': 2},
+            { 'option': 'C', 'number': 3, 'votes': 184000, 'escanyos': 1},
+            { 'option': 'D', 'number': 4, 'votes': 73000, 'escanyos': 0},
+            { 'option': 'E', 'number': 5, 'votes': 27000, 'escanyos': 0},
+            { 'option': 'F', 'number': 6, 'votes': 12000, 'escanyos': 0},
+            { 'option': 'G', 'number': 7, 'votes': 2000, 'escanyos': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_imperiali_3(self):
+        #Test 3: Repartir 0 escaños entre todos los partidos
+        data = {
+            'type': 'COCIENTE_IMPERIALI3',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 391000 },
+                { 'option': 'B', 'number': 2, 'votes': 311000 },
+                { 'option': 'C', 'number': 3, 'votes': 184000 },
+                { 'option': 'D', 'number': 4, 'votes': 73000 },
+                { 'option': 'E', 'number': 5, 'votes': 27000 },
+                { 'option': 'F', 'number': 6, 'votes': 12000 },
+                { 'option': 'G', 'number': 7, 'votes': 2000 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'A', 'number': 1, 'votes': 391000, 'escanyos': 0},
+            { 'option': 'B', 'number': 2, 'votes': 311000, 'escanyos': 0},
+            { 'option': 'C', 'number': 3, 'votes': 184000, 'escanyos': 0},
+            { 'option': 'D', 'number': 4, 'votes': 73000, 'escanyos': 0},
+            { 'option': 'E', 'number': 5, 'votes': 27000, 'escanyos': 0},
+            { 'option': 'F', 'number': 6, 'votes': 12000, 'escanyos': 0},
+            { 'option': 'G', 'number': 7, 'votes': 2000, 'escanyos': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_imperiali_4(self):
+        #Test 4: Repartir escaños para partidos con 0 votos
+        data = {
+            'type': 'COCIENTE_IMPERIALI4',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 0 },
+                { 'option': 'B', 'number': 2, 'votes': 0 },
+                { 'option': 'C', 'number': 3, 'votes': 0 },
+                { 'option': 'D', 'number': 4, 'votes': 0 },
+                { 'option': 'E', 'number': 5, 'votes': 0 },
+                { 'option': 'F', 'number': 6, 'votes': 0 },
+                { 'option': 'G', 'number': 7, 'votes': 0 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'A', 'number': 1, 'votes': 0, 'escanyos': 0},
+            { 'option': 'B', 'number': 2, 'votes': 0, 'escanyos': 0},
+            { 'option': 'C', 'number': 3, 'votes': 0, 'escanyos': 0},
+            { 'option': 'D', 'number': 4, 'votes': 0, 'escanyos': 0},
+            { 'option': 'E', 'number': 5, 'votes': 0, 'escanyos': 0},
+            { 'option': 'F', 'number': 6, 'votes': 0, 'escanyos': 0},
+            { 'option': 'G', 'number': 7, 'votes': 0, 'escanyos': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_imperiali_4(self):
+        #Test 4: Repartir escaños para partidos con 0 votos
+        data = {
+            'type': 'COCIENTE_IMPERIALI4',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 0 },
+                { 'option': 'B', 'number': 2, 'votes': 0 },
+                { 'option': 'C', 'number': 3, 'votes': 0 },
+                { 'option': 'D', 'number': 4, 'votes': 0 },
+                { 'option': 'E', 'number': 5, 'votes': 0 },
+                { 'option': 'F', 'number': 6, 'votes': 0 },
+                { 'option': 'G', 'number': 7, 'votes': 0 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'A', 'number': 1, 'votes': 0, 'escanyos': 0},
+            { 'option': 'B', 'number': 2, 'votes': 0, 'escanyos': 0},
+            { 'option': 'C', 'number': 3, 'votes': 0, 'escanyos': 0},
+            { 'option': 'D', 'number': 4, 'votes': 0, 'escanyos': 0},
+            { 'option': 'E', 'number': 5, 'votes': 0, 'escanyos': 0},
+            { 'option': 'F', 'number': 6, 'votes': 0, 'escanyos': 0},
+            { 'option': 'G', 'number': 7, 'votes': 0, 'escanyos': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_imperiali_5(self):
+        #Test 5: Repartir 0 escaños para partidos con 0 votos
+        data = {
+            'type': 'COCIENTE_IMPERIALI5',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 0 },
+                { 'option': 'B', 'number': 2, 'votes': 0 },
+                { 'option': 'C', 'number': 3, 'votes': 0 },
+                { 'option': 'D', 'number': 4, 'votes': 0 },
+                { 'option': 'E', 'number': 5, 'votes': 0 },
+                { 'option': 'F', 'number': 6, 'votes': 0 },
+                { 'option': 'G', 'number': 7, 'votes': 0 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'A', 'number': 1, 'votes': 0, 'escanyos': 0},
+            { 'option': 'B', 'number': 2, 'votes': 0, 'escanyos': 0},
+            { 'option': 'C', 'number': 3, 'votes': 0, 'escanyos': 0},
+            { 'option': 'D', 'number': 4, 'votes': 0, 'escanyos': 0},
+            { 'option': 'E', 'number': 5, 'votes': 0, 'escanyos': 0},
+            { 'option': 'F', 'number': 6, 'votes': 0, 'escanyos': 0},
+            { 'option': 'G', 'number': 7, 'votes': 0, 'escanyos': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_imperiali_6(self):
+        #Test 6: Partidos con el mismo numero de votos y con 20 escaños a repartir
+        data = {
+            'type': 'COCIENTE_IMPERIALI6',
+            'options': [
+                { 'option': 'A', 'number': 1, 'votes': 10 },
+                { 'option': 'B', 'number': 2, 'votes': 20 },
+                { 'option': 'C', 'number': 3, 'votes': 60 },
+                { 'option': 'D', 'number': 4, 'votes': 20 },
+                { 'option': 'E', 'number': 5, 'votes': 1 },
+                { 'option': 'F', 'number': 6, 'votes': 20 },
+                { 'option': 'G', 'number': 7, 'votes': 0 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'C', 'number': 3, 'votes': 60, 'escanyos': 10},
+            { 'option': 'B', 'number': 2, 'votes': 20, 'escanyos': 3},
+            { 'option': 'D', 'number': 4, 'votes': 20, 'escanyos': 3},
+            { 'option': 'F', 'number': 6, 'votes': 20, 'escanyos': 3},
+            { 'option': 'A', 'number': 1, 'votes': 10, 'escanyos': 1},
+            { 'option': 'E', 'number': 5, 'votes': 1, 'escanyos': 0},
+            { 'option': 'G', 'number': 7, 'votes': 0, 'escanyos': 0},
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
